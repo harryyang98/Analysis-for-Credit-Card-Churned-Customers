40f878075bdc9879bd7186d58cef2c7e8bd440a8
zhaojq <jiaqi_zhao0525@163.com>
2021-03-28 20:36:46 +0800

    fix scale
    problem: unchanged bins for churned/unchurned data

--- js/histogram.js
+++ js/histogram.js
@@ -11,7 +11,7 @@ class Histogram {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 700,
       containerHeight: _config.containerHeight || 350,
-      margin: _config.margin || {top: 25, right: 25, bottom: 20, left: 25},
+      margin: _config.margin || {top: 25, right: 35, bottom: 20, left: 35},
       tooltipPadding: _config.tooltipPadding || 15
     }
     this.data = _data;
@@ -34,7 +34,6 @@ class Histogram {
     // and position it according to the given margin config
     vis.chartArea = vis.svg.append('g')
         .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
-
     vis.chart = vis.chartArea.append('g');
 
     vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
@@ -66,37 +65,45 @@ class Histogram {
     // Prepare data: count number of leaders in each category
     vis.data.sort((a, b) => a[factor] - b[factor]);
     // i.e. [{ key: 'male', count: 10 }, {key: 'female', 20}
-    // const aggregatedDataMap = d3.rollups(vis.data, v => v.length, d => d[factor]);
-    // vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
-    if(this.typeFiltered === null || this.typeFiltered === "unchurned"){
-      const aggregatedDataMap = d3.rollups(vis.unchurned, v => v.length, d => d[factor]);
-      vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
+    console.log(this.typeFiltered);
+    // if(this.typeFiltered === null){
+    //   const aggregatedDataMap = d3.rollups(vis.data, v => v.length, d => d[factor]);
+    //   vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
+    // } else if (this.typeFiltered === "unchurned"){
+    //   const aggregatedDataMap = d3.rollups(vis.unchurned, v => v.length, d => d[factor]);
+    //   vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
    
-      } else if(this.typeFiltered === "churned"){
-        const aggregatedDataMap = d3.rollups(vis.churned, v => v.length, d => d[factor]);
-        vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
-          }
+    //   } else if(this.typeFiltered === "churned"){
+    //     const aggregatedDataMap = d3.rollups(vis.churned, v => v.length, d => d[factor]);
+    //     vis.aggregatedData = Array.from(aggregatedDataMap, ([key, count]) => ({ key, count }));
+    //       }
 
     // Specificy accessor functions
     vis.xValue = d => d.key;
     vis.yValue = d => d.count;
 
-    vis.xScale.domain([d3.min(vis.data, d => d[factor]),d3.max(vis.data, d=>d[factor] )]);
-    //TODO:
-    // vis.yScale.domain([0, d3.max(bins, function(d) { return d.length; })]);
-    vis.yScale.domain([0, d3.max(vis.aggregatedData, vis.yValue)]);
+    if(this.typeFiltered === null){
+      vis.xScale.domain([d3.min(vis.data, d => d[factor]),d3.max(vis.data, d=>d[factor] )]);
+    } else if (this.typeFiltered === "unchurned"){
+      vis.xScale.domain([d3.min(vis.unchurned, d => d[factor]),d3.max(vis.unchurned, d=>d[factor] )]);
+    } else if(this.typeFiltered === "churned"){
+      vis.xScale.domain([d3.min(vis.churned, d => d[factor]),d3.max(vis.churned, d=>d[factor] )]);
+    }
+    // vis.yScale.domain([0, d3.max(vis.aggregatedData, vis.yValue)]);
 
     var histogram = d3.histogram()
     .value(d=>d[factor])   // I need to give the vector of value
     .domain(vis.xScale.domain())  // then the domain of the graphic
     .thresholds(vis.xScale.ticks(24)); // then the numbers of bins
-    if(this.typeFiltered === null || this.typeFiltered === "unchurned"){
+    if(this.typeFiltered === null){
+      vis.bins = histogram(vis.data);
+    }else if (this.typeFiltered === "unchurned"){
     vis.bins = histogram(vis.unchurned);
     } else if(this.typeFiltered === "churned"){
     vis.bins = histogram(vis.churned);
     }
-
-    // console.log(vis.bins); 
+    console.log(vis.bins); 
+    vis.yScale.domain([0, d3.max(vis.bins, function(d) { return d.length; })]);
 
     vis.renderVis();
   }
@@ -105,8 +112,7 @@ class Histogram {
     // Bind data to visual elements, update axes
     let vis = this;
     // console.log(this.typeFiltered);
-    if(this.typeFiltered === null || this.typeFiltered === "unchurned"){
-    //histogram of unchurned
+    //histogram
     vis.chartArea.selectAll("rect")
         .data(vis.bins)
         .join("rect")
@@ -124,29 +130,14 @@ class Histogram {
         .attr('x', vis.width/2)
         .attr('dy', '.71em')
         .style('text-anchor', 'end')
-        .text('unchurned');
-      }
-    //histogram of churned
-    else if(this.typeFiltered ===  "churned"){
-      vis.chartArea.selectAll("rect")
-      .data(vis.bins)
-      .join("rect")
-      .attr("x", 2)
-      .attr("transform", d => {
-        return `translate(${vis.xScale(d.x0)}, ${vis.yScale(d.length)})`
-      })
-      .attr("width", d => vis.xScale(d.x1) - vis.xScale(d.x0))
-      .attr("height", d => vis.height - vis.yScale(d.length))
-      .style("fill", "red")
-
-      vis.chartArea.append('text')
-      .attr('class', 'axis-title')
-      .attr('y', -15)
-      .attr('x', vis.width/2)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
-      .text('churned');
-    }
+        .text(d => {
+          if (this.typeFiltered === null) {
+            return 'total'}
+          else if (this.typeFiltered === "unchurned"){
+            return 'unchurned'}
+          else if (this.typeFiltered ===  "churned"){
+            return 'churned'
+          }});
     
     vis.chartArea.append("circle").attr("cx",vis.width-50).attr("cy",30).attr("r", 6).style("fill", "steelblue").style('opacity',.4)
     vis.chartArea.append("circle").attr("cx",vis.width-50).attr("cy",60).attr("r", 6).style("fill", "red").style('opacity',.4)
