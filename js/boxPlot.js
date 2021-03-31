33669cf1046df9a0648e11a99d1d09c03c816940
sta_c <yuxinch@students.cs.ubc.ca>
2021-03-31 16:20:41 +0800

    adjust margin to show full axis title

--- js/boxPlot.js
+++ js/boxPlot.js
@@ -11,7 +11,7 @@ class BoxPlot {
       containerWidth: 350,
       containerHeight: 350,
       barWidth: 10,
-      margin: { top: 25, right: 10, bottom: 20, left: 30 },
+      margin: { top: 25, right: 10, bottom: 20, left: 65 },
       tooltipPadding: _config.tooltipPadding || 25
     }
     this.data = _data;
